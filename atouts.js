0492fbb146384537a5753bd24e508aadab4c8bd0
AlexisBONNARD <155957821+AlexisBONNARD@users.noreply.github.com>
2026-01-18 18:10:43 +0100

    first commit

---
+++ atouts.js
@@ -0,0 +1,40 @@
+buttons = document.querySelectorAll('.buttAtout')
+divText = document.querySelector('#atout')
+footer = document.querySelector('footer')
+
+footer.style.position = "static"
+
+buttons.forEach(button => {
+
+    button.addEventListener('click',() =>{
+        nom = button.innerText
+        divText.innerHTML = ""
+        footer.style.position = "absolute"
+
+        if (nom == "Atouts Informatiques")
+        {
+            footer.style.position = "static"
+            divText.innerHTML += '<ul><li class="litheme">Langage Informatique :</li><ul class="uldetail">    <li class="lidetail">C# : niveau avancé (POO, création d’API, d’application Console et d’application WPF)</li>    <li class="lidetail">Python : maîtrise pour la création d’algorithmes et d’applications console comme avec interface graphique et IA</li>    <li class="lidetail">HTML/CSS : niveau avancé (création de sites web, application desktop)</li>    <li class="lidetail">Java Script : bases solides (création de sites web, application desktop)</li>    <li class="lidetail">PHP : bases solides (création de sites web)</li></ul><li class="litheme">Framework : </li><ul class="uldetail">    <li class="lidetail"><a class="lien" href="./Competences/realiser1.html">.Net</a> : niveau avancé pour toutes applications en C# et WPF</li>    <li class="lidetail"><a class="lien" href="./Competences/realiser2.html">Laravel</a> : bonnes connaissances du framework PHP</li>    <li class="lidetail"><a class="lien" href="./Competences/realiser3.html">Blazor</a> : niveau avancé pour la création d’un site web</li>    <li class="lidetail"><a class="lien" href="./stagesPro.html">PrestaShop</a> : bonnes connaissances pour la création d’un site web lié à une base de données prédéfinie</li>    <li class="lidetail">Électron : bases solides (création d’application desktop)</li>    <li class="lidetail"><a class="lien" href="./stagesPro.html">Windev</a> : bases solides (application desktop)</li>    <li class="lidetail"><a class="lien" href="./ressources/G3_BONNARD_ALEXIS/Demo.html">Phaser</a> : bases solides (création d’un jeu dans un site web)</li></ul><li class="litheme">Base de données</li><ul class="uldetail">    <li class="lidetail"><a class="lien" href="./Competences/gerer1.html">PostgreSQL</a> : niveau avancé (création et manipulation de base de données, création de requêtes complexes)</li>    <li class="lidetail"><a class="lien" href="./stagesPro.html">NoSQL</a> : bases solides (création et manipulation de données)</li>    <li class="lidetail">SQLite : bases solides (création et manipulation de données)</li></ul></ul>'
+        }
+
+        else if(nom == "Langues étrangères")
+        {
+            divText.innerHTML += '<ul>'
+            divText.innerHTML += '    <li>Anglais : Niveau B1 (scolaire et professionnel – autonomie dans des contextes connus)</li>'
+            divText.innerHTML += '    <li>Espagnol : Niveau A2 (scolaire – échanges simples et quotidiens)</li>'
+            divText.innerHTML += '    <li>Japonais : Niveau A0 (Aucun contact préalable avec la langue — ne comprend ni ne produit de phrases simples.)</li>'
+            divText.innerHTML += '</ul>'
+        }
+
+        else if(nom == "Soft skills")
+        {
+            divText.innerHTML +=  '<ul>'
+            divText.innerHTML +=  '    <li>Curiosité : volonté d’apprendre et de comprendre.</li>'
+            divText.innerHTML +=  '    <li>Travail et esprit d'+"'"+'équipe : interagir et collaborer au sein d'+"'"+'une équipe.</li>'
+            divText.innerHTML +=  '    <li>Organisation : priorisation et organisation des tâches.</li>'
+            divText.innerHTML +=  '</ul>'
+        }
+
+    })
+
+});
---
+++ competence.js
@@ -0,0 +1,119 @@
+buttons = document.querySelectorAll('.buttCompet')
+divText = document.querySelector('#compétences')
+footer = document.querySelector('footer')
+
+buttons.forEach(button => {
+
+    button.addEventListener('click',() =>{
+        nom = button.innerText
+        divText.innerHTML = ""  
+        
+        if (nom == "Réaliser")
+        {
+            footer.style.position = "static"
+            divText.innerHTML += '<h3>Réaliser un développement d’application</h3><h4>Niveau 1 : Développer des applications informatiques simples</h4>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/realiser1.html">AC11.01 | Implémenter des conceptions simples</a></p>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/realiser1.html">AC11.02 | Élaborer des conceptions simples</a></p>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/realiser1.html">AC11.03 | Faire des essais et évaluer leurs résultats en regard des spécifications</a></p>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/realiser1.html">AC11.04 | Développer des interfaces utilisateurs</a></p>'
+
+            divText.innerHTML += "<h4>Niveau 2 : Partir des exigences et aller jusqu'à une application complète</h4>"
+            divText.innerHTML += '<p><a class="lien" href="./Competences/realiser2.html">AC21.01 | Élaborer et implémenter les spécifications fonctionnelles et non fonctionnelles à partir des exigences</a></p>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/realiser2.html">AC21.02 | Appliquer des principes d'+"'accessibilité et d'"+'ergonomie</a></p>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/realiser2.html">AC21.03 | Adopter de bonnes pratiques de conception et de programmation</a></p>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/realiser2.html">AC21.04 | Vérifier et valider la qualité de l’application par les tests</a></p>'
+
+            divText.innerHTML += '<h4>Niveau3 : Adapter des applications sur un ensemble de supports (embarqué, web, mobile, IoT ...)</h4>'
+            divText.innerHTML += '<p>AC31.01 | Choisir et implémenter les architectures adaptées</p>'
+            divText.innerHTML += '<p>AC31.02 | Faire évoluer une application existante</p>'
+            divText.innerHTML += '<p>AC31.03 | Intégrer des solutions dans un environnement de développement, de test ou de production </p>'
+        }
+
+        else if(nom == "Optimiser")
+        {
+            footer.style.position = "static"
+            divText.innerHTML += '<h3>Optimiser des applications informatiques</h3><h4>Niveau 1 : Appréhender et construire des algorithmes</h4>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/optimiser1.html">AC12.01 | Analyser un problème avec méthode (découpage en éléments algorithmiques simples, structure de données...)</a></p>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/optimiser1.html">AC12.02 | Comparer des algorithmes pour des problèmes classiques (tris simples, recherche...)</a></p>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/optimiser1.html">AC12.03 | Formaliser et mettre en œuvre des outils mathématiques pour l’informatique</a></p>'
+
+            divText.innerHTML += '<h4>Niveau 2 : Sélectionner les algorithmes adéquats pour répondre à un problème donné</h4>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/optimiser2.html">AC22.01 | Choisir des structures de données complexes adaptées au problème</a></p>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/optimiser2.html">AC22.02 | Utiliser des techniques algorithmiques adaptées pour des problèmes complexes (par ex. recherche opérationnelle, méthodes arborescentes, optimisation globale, intelligence artificielle...)</a></p>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/optimiser2.html">AC22.03 | Comprendre les enjeux et moyens de sécurisation des données et du code</a></p>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/optimiser2.html">AC22.04 | Évaluer l’impact environnemental et sociétal des solutions proposées</a></p>'
+
+            divText.innerHTML += '<h4>Niveau 3 : Analyser et optimiser des applications</h4>'
+            divText.innerHTML += "<p>AC32.01 | Anticiper les résultats de diverses métriques (temps d’exécution, occupation mémoire, …) </p>"
+            divText.innerHTML += "<p>AC32.02 | Profiler, analyser et justifier le comportement d’un code existant </p>"
+            divText.innerHTML += "<p>AC32.03 | Choisir et utiliser des bibliothèques et méthodes dédiées au domaine d’application (imagerie, immersion, IA, jeux vidéo, parallélisme, …) </p>"
+        }
+
+        else if(nom == "Administrer")
+        {
+            footer.style.position = "absolute"
+            divText.innerHTML += '<h3>Administrer des systèmes informatiques communicants complexes</h3><h4>Niveau 1 : Installer et configurer un poste de travail</h4>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/administrer1.html">AC13.01 | Identifier les différents composants (matériels et logiciels) d’un système numérique</a></p>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/administrer1.html">AC13.02 | Utiliser les fonctionnalités de base d’un système multitâches / multiutilisateurs</a></p>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/administrer1.html">AC13.03 | Installer et configurer un système d’exploitation et des outils de développement</a></p>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/administrer1.html">AC13.04 | Configurer un poste de travail dans un réseau d’entreprise</a></p>'
+
+            divText.innerHTML += '<h4>Niveau 2 : Déployer des services dans une architecture réseau</h4>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/administrer2.html">AC23.01 | Concevoir et développer des applications communicantes</a></p>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/administrer2.html">AC23.02 | Utiliser des serveurs et des services réseaux virtualisés</a></p>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/administrer2.html">AC23.03 | Sécuriser les services et données d’un système</a></p>'
+        }
+
+        else if(nom == "Gérer")
+        {
+            footer.style.position = "absolute"
+            divText.innerHTML += "<h3>Gérer des données de l’information</h3><h4>Niveau 1 : Concevoir et mettre en place une base de données à partir d'un cahier des charges client</h4>"
+            divText.innerHTML += '<p><a class="lien" href="./Competences/gerer1.html">AC14.01 | Mettre à jour et interroger une base de données relationnelle (en requêtes directes ou à travers une application)</a></p>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/gerer1.html">AC14.02 | Visualiser des données</a></p>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/gerer1.html">AC14.03 | Concevoir une base de données relationnelle à partir d’un cahier des charges</a></p>'
+
+            divText.innerHTML += '<h4>Niveau 2 : Optimiser une base de données, interagir avec une application et mettre en oeuvre la sécurité</h4>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/gerer2.html">AC24.01 | Optimiser les modèles de données de l’entreprise</a></p>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/gerer2.html">AC24.02 | Assurer la sécurité des données (intégrité et confidentialité)</a></p>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/gerer2.html">AC24.03 | Organiser la restitution de données à travers la programmation et la visualisation</a></p>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/gerer2.html">AC24.04 | Manipuler des données hétérogènes</a></p>'        
+        }
+
+        else if(nom == "Conduire")
+        {
+            footer.style.position = "absolute"
+            divText.innerHTML += '<h3>Conduire un projet informatique</h3><h4>Niveau 1 : Identifier les besoins métiers des clients et des utilisateurs</h4>'
+            divText.innerHTML += "<p>"+'<a class="lien" href="./Competences/conduire1.html">'+"AC15.01 | Appréhender les besoins du client et de l'utilisateur</a></p>"
+            divText.innerHTML += '<p><a class="lien" href="./Competences/conduire1.html">AC15.02 | Mettre en place les outils de gestion de projet</a></p>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/conduire1.html">AC15.03 | Identifier les acteurs et les différentes phases d’un cycle de développement</a></p>'
+            
+            divText.innerHTML += '<h4>Niveau 2 : Appliquer une démarche de suivi de projet en fonction des besoins métiers des clients et des utilisateurs</h4>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/conduire2.html">AC25.01 | Identifier les processus présents dans une organisation en vue d’améliorer les systèmes d’information</a></p>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/conduire1.html">AC25.02 | '+"Formaliser les besoins du client et de l'utilisateur"+'</a></p>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/conduire2.html">AC25.03 | Identifier les critères de faisabilité d’un projet informatique</a></p>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/conduire2.html">AC25.04 | Définir et mettre en œuvre une démarche de suivi de projet</a></p>'
+        }
+
+        else if(nom == "Collaborer")
+        {
+            footer.style.position = "static"
+            divText.innerHTML += '<h3>Collaborer / Travailler dans une équipe informatique</h3><h4>Niveau 1 : Identifier ses aptitudes pour travailler dans une équipe</h4>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/collaborer1.html">AC16.01 | Appréhender l’écosystème numérique</a></p>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/collaborer1.html">AC16.02 | Découvrir les aptitudes requises selon les différents secteurs informatiques</a></p>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/collaborer1.html">AC16.03 | Identifier les statuts, les fonctions et les rôles de chaque membre d’une équipe pluridisciplinaire</a></p>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/collaborer1.html">AC16.04 | Acquérir les compétences interpersonnelles pour travailler en équipe</a></p>'
+
+            divText.innerHTML += "<h4>Niveau 2 : Situer son rôle et ses missions au sein d'une équipe informatique</h4>"
+            divText.innerHTML += '<p><a class="lien" href="./Competences/collaborer2.html">AC26.01 | Comprendre la diversité, la structure et la dimension de l’informatique dans une organisation (ESN, DSI,...)</a></p>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/collaborer2.html">AC26.02 | Appliquer une démarche pour intégrer une équipe informatique au sein d’une organisation</a></p>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/collaborer2.html">AC26.03 | Mobiliser les compétences interpersonnelles pour travailler dans une équipe informatique</a></p>'
+            divText.innerHTML += '<p><a class="lien" href="./Competences/collaborer2.html">AC26.04 | Rendre compte de son activité professionnelle</a></p>'
+
+            divText.innerHTML += '<h4>Niveau 3 : Manager une équipe informatique</h4>'
+            divText.innerHTML += '<p>AC36.01 | Organiser et partager une veille numérique</p>'
+            divText.innerHTML += '<p>AC36.02 | Identifier les principaux coûts et gains de la (ou des) mission(s) </p>'
+            divText.innerHTML += '<p>AC36.03 | Guider la conduite du changement informatique au sein d’une organisation</p>'
+            divText.innerHTML += '<p>AC36.04 | Accompagner le management de projet informatique</p>'
+        }
+    })
+});
